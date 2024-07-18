aadd18bcd9546882fbb73d9fa0a89eff50022c6b
nckedev <nmk411@gmail.com>
2024-07-18 17:12:27 +0200

    PartialOrd for SourceIndex

--- src/source_char.rs
+++ src/source_char.rs
@@ -54,9 +54,56 @@ impl SourceIndex {
     }
 }
 
+impl PartialOrd for SourceIndex {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.row == other.row {
+            return Some(self.col.cmp(&other.col));
+        } else {
+            return Some(self.row.cmp(&other.row));
+        }
+    }
+}
+
 pub(crate) enum SourceIndexTarget {
     NextCol,
     NextRow,
     Any,
     AnyForward,
+}
+
+#[cfg(test)]
+mod source_index_tests {
+    use super::*;
+
+    #[test]
+    fn partial_ord_row() {
+        let bigger = SourceIndex { row: 10, col: 20 };
+
+        let smaller = SourceIndex { row: 5, col: 10 };
+
+        assert!(bigger > smaller);
+        assert!(smaller < bigger);
+        assert_ne!(smaller, bigger);
+    }
+
+    #[test]
+    fn partial_ord_same_row() {
+        let bigger = SourceIndex { row: 10, col: 20 };
+
+        let smaller = SourceIndex { row: 10, col: 10 };
+
+        assert!(bigger > smaller);
+        assert!(smaller < bigger);
+        assert_ne!(smaller, bigger);
+    }
+    #[test]
+    fn partial_ord_eq() {
+        let bigger = SourceIndex { row: 10, col: 10 };
+
+        let smaller = SourceIndex { row: 10, col: 10 };
+
+        assert_eq!(false, bigger > smaller);
+        assert_eq!(false, smaller < bigger);
+        assert_eq!(smaller, bigger);
+    }
 }
